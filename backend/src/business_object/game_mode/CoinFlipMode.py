dc65d9d2d733da5d94bc81b4489c07bb033c1bad
anastacia <anastacia.calvel@eleve.ensai.fr>
2026-09-04 10:42:28 +0000

    gamefactory comit

--- backend/src/business_object/game_mode/CoinFlipMode.py
+++ backend/src/business_object/game_mode/CoinFlipMode.py
@@ -2,9 +2,13 @@ import random
 from typing import TYPE_CHECKING
 
 from business_object.game import Game
+from business_object.game_mode.GameMode import JeuStandard
 from business_object.player import Player
-from business_object.game_mode.GameMode import JeuStandard 
 
 if TYPE_CHECKING:
     from business_object.game import Game
     from business_object.player import Player
+
+class CoinFlipMode(JeuStandard):
+    def play(self, p1 : Player, p2 : Player, **kwags) -> Game:
+        
--- backend/src/business_object/game_mode/GameMode.py
+++ backend/src/business_object/game_mode/GameMode.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import Type, Union
 
 from business_object.game import Game
 from business_object.player import Player
+from business_object.game_mode import CoinFlipMode
+from business_object.game_mode import DiceMode
 
 
 class JeuStandard(ABC):
@@ -13,3 +16,23 @@ class JeuStandard(ABC):
         Prends deux joueurs et retourne une instance de jeu
         """
         pass
+
+class GameModeFactory:
+
+    @classmethod
+    def get_mode(cls,game_mode: str) -> Game:
+        """
+        Returns the corresponding GameMode object.
+        Args:
+            game_mode (str): The identifier of the game mode (e.g., 'coinflip', 'dice').
+        Returns:
+            GameMode: An instance of a class implementing GameMode.
+        Raises:
+            ValueError: If the requested game_mode is not supported.
+        """
+        if game_mode == "coinflip":
+            return CoinFlip()
+        elif game_mode == "lancé de dés":
+            return DiceMode()
+        else:
+            raise ValueError(f"Le jeu {game_mode} n'existe pas")
