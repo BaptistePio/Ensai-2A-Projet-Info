23b8d5e921a978727addd3daf07504880a44c706
sarahpalenne12 <sarah.palenne@eleve.ensai.fr>
2026-09-18 11:48:56 +0000

    TP4 sarah palenne

--- backend/src/client/game_client.py
+++ backend/src/client/game_client.py
@@ -1,5 +1,23 @@
 import requests
+from game_mode.player import Player
 
-def get_game():
-    r = requests.get('https://github.com/BaptistePio/Ensai-2A-Projet-Info/tree/sarahpalenne-tp4/data/game.json')
-    return r 
+
+def get_games(id_game, player1, player2, winner, description, timestamp, game_mode):
+    url = "https://github.com/BaptistePio/Ensai-2A-Projet-Info/blob/sarahpalenne-tp4/data/game.json"
+    payload = {
+        "id": id_game,
+        "players_list": [
+            player1,
+            player2
+            ],
+        "winner_name": winner ,
+        "details": description,
+        "duration_seconds": timestamp,
+        "mode_type": game_mode
+  }
+    r = requests.get(
+        url,
+        json = playload
+        )
+    return r.json()
+
